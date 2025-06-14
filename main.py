943c566e5388600adbaec2c018ec1d91e7ec5300
ItencY <itency@outlook.com>
2025-06-14 15:04:47 +0300

    change to input to promt

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
+
+if len(sys.argv) < 2:
+    print("Prompt is not provided")
+    sys.exit(1)
+promt = sys.argv[1]
+print(promt)
+
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.0-flash-001", contents=promt
 )
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
