48aa8e81e47b6d63ab229af2d876df4c1c7bd696
ItencY <itency@outlook.com>
2025-06-15 08:42:22 +0300

    store user`s promt

--- main.py
+++ main.py
@@ -2,20 +2,32 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
+def main():
+    load_dotenv()
 
-if len(sys.argv) < 2:
-    print("Prompt is not provided")
-    sys.exit(1)
-promt = sys.argv[1]
-print(promt)
+    if len(sys.argv) < 2:
+        print("Prompt is not provided")
+        sys.exit(1)
+    promt = sys.argv[1]
 
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=promt
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=promt)]),
+    ]
+
+    generate_content(client, messages)
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+    model="gemini-2.0-flash-001", contents=messages
 )
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
