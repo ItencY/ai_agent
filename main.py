a38f75df41dd5030b86d06da6c6cf39632b00664
ItencY <itency@outlook.com>
2025-06-18 10:34:43 +0300

    update imports

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
 from call_function import call_function, available_functions
 from config import MAX_ITERS
 
